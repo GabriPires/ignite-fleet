6f66e145d1105d552183e4d4a6709666c249292d
Gabriel Pires <gabpiresdev@gmail.com>
2023-10-18 16:31:12 -0300

    feat: custom fonts and loading indicator

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -3,5 +3,6 @@ import theme from '../theme'
 
 declare module 'styled-components' {
   type Theme = typeof theme
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends Theme { }
 }
