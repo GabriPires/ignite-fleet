c063cd6f0526c7cde04a8a1387041963732f7217
Gabriel Pires <gabpiresdev@gmail.com>
2023-10-18 16:16:34 -0300

    chore: add assets to application and setup eslint

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,10 +1,10 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
     justifyContent: 'center',
   },
-});
+})
