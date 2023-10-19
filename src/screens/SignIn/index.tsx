6a2ddb62a8ecda9f90c6ec911806248d377cfdbf
Gabriel Pires <gabpiresdev@gmail.com>
2023-10-19 10:57:52 -0300

    feat: fetching user profile from google api

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,8 @@
-import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env'
+import { IOS_CLIENT_ID } from '@env'
 import * as Google from 'expo-auth-session/providers/google'
 import * as WebBrowser from 'expo-web-browser'
 import { useEffect, useState } from 'react'
+import { Alert } from 'react-native'
 import backgroundImg from '../../assets/background.png'
 import { Button } from '../../components/Button'
 import { Container, Slogan, Title } from './styles'
@@ -12,7 +13,8 @@ export function SignIn() {
   const [isAuthenticating, setIsAuthenticating] = useState(false)
 
   const [, response, googleSignIn] = Google.useAuthRequest({
-    androidClientId: ANDROID_CLIENT_ID,
+    // temporary workaround for android client id don't work
+    androidClientId: IOS_CLIENT_ID,
     iosClientId: IOS_CLIENT_ID,
     scopes: ['profile', 'email'],
   })
@@ -30,7 +32,17 @@ export function SignIn() {
   useEffect(() => {
     if (response?.type === 'success') {
       if (response.authentication?.idToken) {
-        console.log(response.authentication.idToken)
+        fetch(
+          `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${response.authentication.idToken}`,
+        )
+          .then(async (res) => res.json())
+          .then(console.log)
+      } else {
+        Alert.alert(
+          'Entrar',
+          'Não foi possível conectar com a sua conta do Google',
+        )
+        setIsAuthenticating(false)
       }
     }
   }, [response])
