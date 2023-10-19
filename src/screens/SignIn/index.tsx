abf7a418a2daa99f110c1bacb83601310b6e8993
Gabriel Pires <gabpiresdev@gmail.com>
2023-10-19 09:53:28 -0300

    feat: sign in screen with google login in external navigator

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,14 +1,49 @@
-import { Container, Slogan, Title } from './styles'
-
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env'
+import * as Google from 'expo-auth-session/providers/google'
+import * as WebBrowser from 'expo-web-browser'
+import { useEffect, useState } from 'react'
 import backgroundImg from '../../assets/background.png'
 import { Button } from '../../components/Button'
+import { Container, Slogan, Title } from './styles'
+
+WebBrowser.maybeCompleteAuthSession()
 
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  const [, response, googleSignIn] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ['profile', 'email'],
+  })
+
+  function handleSignInWithGoogle() {
+    setIsAuthenticating(true)
+
+    googleSignIn().then((response) => {
+      if (response.type !== 'success') {
+        setIsAuthenticating(false)
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      if (response.authentication?.idToken) {
+        console.log(response.authentication.idToken)
+      }
+    }
+  }, [response])
+
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
       <Slogan>Gestão de uso de veículos</Slogan>
-      <Button title="Entrar com Google" />
+      <Button
+        title="Entrar com Google"
+        isLoading={isAuthenticating}
+        onPress={handleSignInWithGoogle}
+      />
     </Container>
   )
 }
