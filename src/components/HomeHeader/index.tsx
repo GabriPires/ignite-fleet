8dd05846d22b3a847b1cb4ec569159def4c350be
Gabriel Pires <gabpiresdev@gmail.com>
2023-10-19 12:12:25 -0300

    feat: use user profile data from realm authenticate context

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,18 +1,21 @@
+import { useUser } from '@realm/react'
 import { Power } from 'phosphor-react-native'
 import { TouchableOpacity } from 'react-native'
 import theme from '../../theme'
 import { Container, Greeting, Message, Name, Picture } from './styles'
 
 export function HomeHeader() {
+  const user = useUser()
+
   return (
     <Container>
       <Picture
-        source={{ uri: 'https://github.com/GabriPires.png' }}
+        source={{ uri: user.profile.pictureUrl }}
         placeholder="L184i9ofbHof00ayjsay~qj[ayj@"
       />
       <Greeting>
         <Message>Olá,</Message>
-        <Name>Gabriel</Name>
+        <Name>{user.profile.name}</Name>
       </Greeting>
 
       <TouchableOpacity>
