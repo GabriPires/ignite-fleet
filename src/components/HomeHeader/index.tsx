0767d4be68dc99ce17f7d5acebd59c4c02b0c879
Gabriel Pires <gabpiresdev@gmail.com>
2023-10-19 12:08:13 -0300

    feat: new picture component to home header with expo image

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,11 +1,15 @@
 import { Power } from 'phosphor-react-native'
 import { TouchableOpacity } from 'react-native'
 import theme from '../../theme'
-import { Container, Greeting, Message, Name } from './styles'
+import { Container, Greeting, Message, Name, Picture } from './styles'
 
 export function HomeHeader() {
   return (
     <Container>
+      <Picture
+        source={{ uri: 'https://github.com/GabriPires.png' }}
+        placeholder="L184i9ofbHof00ayjsay~qj[ayj@"
+      />
       <Greeting>
         <Message>Olá,</Message>
         <Name>Gabriel</Name>
