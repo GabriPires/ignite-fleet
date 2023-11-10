01eb853f9db3c14c4bd8bb0bf4cb8530349e736e
Gabriel Pires <gabpiresdev@gmail.com>
2023-11-10 11:09:46 -0300

    feat: add vehicle in use information to Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,22 +1,46 @@
 import { useNavigation } from '@react-navigation/native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Alert } from 'react-native'
 import { CarStatus } from '../../components/CarStatus'
 import { HomeHeader } from '../../components/HomeHeader'
+import { useQuery } from '../../libs/realm'
+import { Historic } from '../../libs/realm/schemas/Historic'
 import { Container, Content } from './styles'
 
 export function Home() {
+  const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null)
+
   const { navigate } = useNavigation()
+
+  const historic = useQuery(Historic)
 
   function handleRegisterMovement() {
     navigate('departure')
   }
+
+  function fetchVehicle() {
+    try {
+      const vehicle = historic.filtered("status = 'departure'")[0]
+      setVehicleInUse(vehicle || null)
+    } catch (error) {
+      Alert.alert('Veículo em uso', 'Não foi possível buscar o veículo em uso.')
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchVehicle()
+  }, [])
 
   return (
     <Container>
       <HomeHeader />
 
       <Content>
-        <CarStatus onPress={handleRegisterMovement} />
+        <CarStatus
+          licensePlate={vehicleInUse?.license_plate}
+          onPress={handleRegisterMovement}
+        />
       </Content>
     </Container>
   )
