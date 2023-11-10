b0ed0a654833b30c077b2da70b6b9fda61538ba0
Gabriel Pires <gabpiresdev@gmail.com>
2023-11-10 12:09:14 -0300

    feat: add cancel vehicle usage functionality to Arrival screen

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,11 +1,12 @@
-import { useRoute } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import { X } from 'phosphor-react-native'
 import React from 'react'
+import { Alert } from 'react-native'
 import { BSON } from 'realm'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
 import { IconButton } from '../../components/IconButton'
-import { useObject } from '../../libs/realm'
+import { useObject, useRealm } from '../../libs/realm'
 import { Historic } from '../../libs/realm/schemas/Historic'
 import {
   Container,
@@ -23,8 +24,30 @@ interface ArrivalScreenParamsProps {
 export function Arrival() {
   const route = useRoute()
   const { id } = route.params as ArrivalScreenParamsProps
+  const { goBack } = useNavigation()
 
   const historic = useObject(Historic, new BSON.UUID(id) as unknown as string)
+  const realm = useRealm()
+
+  function handleCancelVehicleUsage() {
+    Alert.alert('Cancelar', 'Cancelar a utilização do veículo?', [
+      {
+        text: 'Não',
+        style: 'cancel',
+      },
+      {
+        text: 'Sim',
+        onPress: () => removeVehicleUsage(),
+      },
+    ])
+  }
+
+  function removeVehicleUsage() {
+    realm.write(() => {
+      realm.delete(historic)
+      goBack()
+    })
+  }
 
   return (
     <Container>
@@ -38,7 +61,7 @@ export function Arrival() {
         <Description>{historic?.description}</Description>
 
         <Footer>
-          <IconButton icon={X} />
+          <IconButton icon={X} onPress={handleCancelVehicleUsage} />
           <Button title="Registrar chegada" />
         </Footer>
       </Content>
