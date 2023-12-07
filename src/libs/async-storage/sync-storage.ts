cf5eac135d3d62707972993375be088adc4cda31
Gabriel Pires <gabpiresdev@gmail.com>
2023-12-07 12:03:06 -0300

    feat: setup and create save and get functions with async storage to handle with sync date

---
+++ src/libs/async-storage/sync-storage.ts
@@ -0,0 +1,16 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const ASYNC_STORAGE_KEY = '@ignitefleet:last_sync'
+
+export async function saveLastSyncTimeStamp() {
+  const timeStamp = new Date().getTime()
+  await AsyncStorage.setItem(ASYNC_STORAGE_KEY, timeStamp.toString())
+
+  return timeStamp
+}
+
+export async function getLastSyncTimeStamp() {
+  const timeStamp = await AsyncStorage.getItem(ASYNC_STORAGE_KEY)
+
+  return timeStamp ? Number(timeStamp) : null
+}
