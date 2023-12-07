a2b36d202d7649a10039bb6a6269adac38265f5a
Gabriel Pires <gabpiresdev@gmail.com>
2023-12-07 11:07:01 -0300

    feat: top message component

--- App.tsx
+++ App.tsx
@@ -8,10 +8,12 @@ import {
   useFonts,
 } from '@expo-google-fonts/roboto'
 import { AppProvider, UserProvider } from '@realm/react'
+import { WifiSlash } from 'phosphor-react-native'
 import { StatusBar } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { ThemeProvider } from 'styled-components/native'
 import { Loading } from './src/components/Loading'
+import { TopMessage } from './src/components/TopMessage'
 import { RealmProvider, syncConfig } from './src/libs/realm'
 import { Routes } from './src/routes'
 import { SignIn } from './src/screens/SignIn'
@@ -38,6 +40,9 @@ export default function App() {
             backgroundColor="transparent"
             translucent
           />
+
+          <TopMessage title="Você está offline" icon={WifiSlash} />
+
           <UserProvider fallback={<SignIn />}>
             <RealmProvider sync={syncConfig} fallback={Loading}>
               <Routes />
---
+++ src/components/TopMessage/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { useTheme } from 'styled-components/native'
+import { IconBoxProps } from '../IconButton'
+import { Container, Title } from './styles'
+
+interface Props {
+  icon?: IconBoxProps
+  title: string
+}
+
+export function TopMessage({ title, icon: Icon }: Props) {
+  const theme = useTheme()
+  const insets = useSafeAreaInsets()
+
+  const paddingTop = insets.top + 5
+
+  return (
+    <Container style={{ paddingTop }}>
+      {Icon && <Icon size={24} color={theme.COLORS.BRAND_MID} />}
+      <Title>{title}</Title>
+    </Container>
+  )
+}
