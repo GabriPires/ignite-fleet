94e7d182950530f4481b157037118ada9f6df796
GabriPires <gabpiresdev@gmail.com>
2024-01-03 18:57:17 -0300

    feat: new location info component and show it on departure screen

---
+++ src/components/LocationInfo/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Description, Info, Label } from './styles'
+
+export type LocationInfoProps = {
+  label: string
+  description: string
+}
+
+export function LocationInfo({ description, label }: LocationInfoProps) {
+  return (
+    <Container>
+      <Info>
+        <Label numberOfLines={1}>{label}</Label>
+        <Description numberOfLines={1}>{description}</Description>
+      </Info>
+    </Container>
+  )
+}
