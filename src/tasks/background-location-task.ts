3108e298cdfd024469ae691452b410856099f2a4
GabriPires <gabpiresdev@gmail.com>
2024-01-05 09:55:39 -0300

    feat: new function to stop location tracking task

--- src/tasks/background-location-task.ts
+++ src/tasks/background-location-task.ts
@@ -1,4 +1,8 @@
-import { Accuracy, startLocationUpdatesAsync } from 'expo-location'
+import {
+  Accuracy,
+  startLocationUpdatesAsync,
+  stopLocationUpdatesAsync,
+} from 'expo-location'
 import * as TaskManager from 'expo-task-manager'
 
 export const BACKGROUND_LOCATION_TASK_NAME = 'location-tracking'
@@ -28,11 +32,33 @@ TaskManager.defineTask(
 
 export async function startLocationTrackingTask() {
   try {
+    const hasStarted = await TaskManager.isTaskRegisteredAsync(
+      BACKGROUND_LOCATION_TASK_NAME,
+    )
+
+    if (hasStarted) {
+      await stopLocationTrackingTask()
+    }
+
     await startLocationUpdatesAsync(BACKGROUND_LOCATION_TASK_NAME, {
       accuracy: Accuracy.Highest,
       distanceInterval: 1,
       timeInterval: 1000,
     })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function stopLocationTrackingTask() {
+  try {
+    const hasStarted = await TaskManager.isTaskRegisteredAsync(
+      BACKGROUND_LOCATION_TASK_NAME,
+    )
+
+    if (hasStarted) {
+      await stopLocationUpdatesAsync(BACKGROUND_LOCATION_TASK_NAME)
+    }
   } catch (error) {
     console.log(error)
   }
