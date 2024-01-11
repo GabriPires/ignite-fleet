0327b881f4e5fe320a9f946c4559ab5a12a74870
GabriPires <gabpiresdev@gmail.com>
2024-01-11 09:54:38 -0300

    feat: new coords schema for realm

--- src/libs/realm/schemas/Historic.ts
+++ src/libs/realm/schemas/Historic.ts
@@ -1,10 +1,13 @@
 import { Realm } from '@realm/react'
 import { ObjectSchema } from 'realm'
 
+import { CoordsSchemaProps } from './Coords'
+
 interface GenerateProps {
   user_id: string
   license_plate: string
   description: string
+  coords: CoordsSchemaProps[]
 }
 
 export class Historic extends Realm.Object<Historic> {
@@ -13,16 +16,23 @@ export class Historic extends Realm.Object<Historic> {
   license_plate!: string
   description!: string
   status!: string
+  coords!: CoordsSchemaProps[]
   created_at!: Date
   updated_at!: Date
 
-  static generate({ user_id, license_plate, description }: GenerateProps) {
+  static generate({
+    user_id,
+    license_plate,
+    description,
+    coords,
+  }: GenerateProps) {
     return {
       _id: new Realm.BSON.UUID(),
       user_id,
       license_plate,
       description,
       status: 'departure',
+      coords,
       created_at: new Date(),
       updated_at: new Date(),
     }
@@ -40,6 +50,10 @@ export class Historic extends Realm.Object<Historic> {
       license_plate: 'string',
       description: 'string',
       status: 'string',
+      coords: {
+        type: 'list',
+        objectType: 'Coords',
+      },
       created_at: 'date',
       updated_at: 'date',
     },
